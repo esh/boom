495a1535ce4b7ca208cfeb463c6530b3c80a8f97
Edward Chan <chan@sikho.ca>
2011-06-26 16:38:26 +0900

    why doesn't it work?

--- utils.js
+++ utils.js
@@ -53,3 +53,17 @@ function degToRad(degrees) {
 	return degrees * Math.PI / 180;
 }
 
+function crossProduct(a, b) {
+	return [a[1] * b[2] - a[2] * b[1],
+		a[2] * b[0] - a[0] * b[2],
+		a[0] * b[1] - a[1] * b[0]]
+}
+
+function dotProduct(a, b) {
+	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
+}
+
+function normalize(v) {
+	var l = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
+	return [v[0] / l, v[1] / l, v[2] / l]
+}
