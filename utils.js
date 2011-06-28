ea514096b767839555d3915fac40fcb506536588
Edward Chan <chan@sikho.ca>
2011-06-28 20:32:37 +0900

    retire dot and normalize

--- utils.js
+++ utils.js
@@ -37,15 +37,6 @@ function degToRad(degrees) {
 	return degrees * Math.PI / 180;
 }
 
-function dotProduct(a, b) {
-	return a[0] * b[0] + a[1] * b[1] + a[2] * b[2]
-}
-
-function normalize(v) {
-	var l = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2])
-	return [v[0] / l, v[1] / l, v[2] / l]
-}
-
 function fastSqrt(number) {
 	var x = number; 
 	while (Math.abs(x*x-number) > 0.001) x = (x*x+number)/(2*x)
